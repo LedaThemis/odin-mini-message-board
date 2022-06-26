353a30642bfecc4c2f2c5911597ede9f78dd0a96
Leda <ledathemis@protonmail.com>
2022-06-26 19:52:58 +0000

    add logic to handle adding new messages

--- routes/index.js
+++ routes/index.js
@@ -3,21 +3,26 @@ var router = express.Router();
 
 const messages = [
   {
-    text: "Hi there!",
-    user: "Danny",
-    added: new Date()
+    text: 'Hi there!',
+    user: 'Danny',
+    added: new Date(),
   },
   {
-    text: "Hi!",
-    user: "Curry",
-    added: new Date()
-  }
+    text: 'Hi!',
+    user: 'Curry',
+    added: new Date(),
+  },
 ];
 
-
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Mini Messageboard | Odin', messages: messages });
+});
+
+router.post('/new', (req, res, next) => {
+  const { messageUser, messageText } = req.body;
+  messages.push({ text: messageText, user: messageUser, added: new Date() });
+  res.redirect('/');
 });
 
 module.exports = router;
