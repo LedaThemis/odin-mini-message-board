5d735acd14c07060a5838050bdb6321f0ce60fc5
Leda <ledathemis@protonmail.com>
2022-06-26 19:38:17 +0000

    add messages array

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Danny",
+    added: new Date()
+  },
+  {
+    text: "Hi!",
+    user: "Curry",
+    added: new Date()
+  }
+];
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Mini Messageboard | Odin', messages: messages });
 });
 
 module.exports = router;
